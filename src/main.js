5236a42991432ef8da3249711f278dc213c7ffd7
Jason Breedlove <j@jasonbreedlove.com>
2025-05-07 13:35:42 -0700

    Add interactive mouse constraint and scene boundaries
    Integrated `MouseConstraint` for interactive physics manipulation, allowing users to interact with objects using the mouse. Added static walls to define scene boundaries and updated object initialization for a dynamic demo setup.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Matter from 'matter-js';
-const { Engine, Render, Runner, World, Bodies} = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
 
 const engine = Engine.create();
 const {world} = engine;
@@ -13,7 +13,21 @@ const render = Render.create({
 })
 Render.run(render);
 Runner.run(Runner.create(), engine);
+World.add(world, MouseConstraint.create(engine,{
+    mouse:Mouse.create(render.canvas)
+}))
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {isStatic:true})
-World.add(world, shape);
+const shape = Bodies.rectangle(200, 200, 50, 50,{isStatic:true} )
+// World.add(world, shape);
+
+// walls
+const walls = [
+    Bodies.rectangle(400, 0, 800, 40, {isStatic:true}),
+    Bodies.rectangle(400, 600, 800, 40, {isStatic:true}),
+    Bodies.rectangle(0, 300, 40, 600, {isStatic:true}),
+    Bodies.rectangle(800, 300, 40, 6000, {isStatic:true}),
+
+]
+World.add(world, walls);
+World.add(world, Bodies.rectangle(200, 200, 50, 50, {isStatic:false}))
 
